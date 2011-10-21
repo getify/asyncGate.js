4d62805406d58e365909d8af75cc9e033b624642
Kyle Simpson <getify@gmail.com>
2011-10-21 14:19:52 -0500

    initial v0.1

--- README.md
+++ README.md
@@ -2,6 +2,57 @@
 
 A lightweight API for performing parallel async tasks with a "gate" waiting for all to finish before proceeding.
 
+## Explanation
+
+Say you want do two or more asynchronous tasks in parallel (like `setTimeout` delays, XHR calls, etc). You want to easily listen for when all these different asynchronous tasks have completed. You need a gate.
+
+You create a gate by calling `$AG(...)`. **Each time you call `$AG()`, you create a new, separate gate.**
+
+To each gate, you pass in one or more functions, **which $AG will execute for you**. To each function that $AG executes, it will pass as the first parameter a trigger function. You simply need to make sure this trigger function is called at some point later, when your task is complete. NOTE: your tasks can be either synchronous (completing immediately) or asynchronous; it makes no difference to $AG.
+
+To listen for when all previously specified functions have completed, and the gate can then be "opened" (metaphorically speaking), simply call `then(...)` on your gate to register a completion callback. You can call `then()` as many times as you would like. If you call `then()` on a gate that is already completed, the callback you specify will just be executed immediately.
+
+If you want to add more asynchronously-completing functions to your gate to have it wait on, simply call `and(...)` on your gate. NOTE: You cannot call `and()` after you have called `then()` to register your completion callback(s).
+
+## Usage Examples
+
+Using the following example setup:
+
+    function fn1(done) {
+       setTimeout(done,1000);
+    }
+    
+    function fn2(done) {
+       setTimeout(done,2000);
+    }
+    
+    function yay() {
+       alert("Done!");
+    }
+
+Execute `fn1` and `fn2` in parallel, then call `yay` when complete:
+
+    $AG(fn1,fn2).then(yay);
+    
+    // same as above
+    $AG([fn1,fn2]).then(yay);
+    
+    // same as above    
+    $AG([fn1],[[[fn2]]]).then(yay);
+
+Execute `fn1`, then add `fn2` to be executed, then call `yay` when complete:
+
+    $AG(fn1).and(fn2).then(yay);
+    
+    // same as above
+    var gate = $AG(fn1);
+    // later
+    gate.and(fn2);
+    // later
+    gate.then(yay);
+
+
+
 ## License 
 
 (The MIT License)
--- ag.src.js
+++ ag.src.js
@@ -1,3 +1,8 @@
+/*! asyncGate.js
+    v0.1 (c) Kyle Simpson
+    MIT License: http://getify.mit-license.org
+*/
+
 (function(global){
 
   var old_$AG = global.$AG;
@@ -7,6 +12,27 @@
 
 	// test for array
 	function is_array(arr) { return Object.prototype.toString.call(arr) == "[object Array]"; }
+	
+	// flatten array
+	function flatten_array(arr) {
+    var i, tmp;
+    
+    for (i=0; i<arr.length; i++) {
+      if (is_array(arr[i])) {
+        // recursion! yay!
+        tmp = flatten_array(arr[i]);
+        
+        // prepend `splice()` arguments to `tmp` array, to enable `apply()` call
+        tmp.unshift(i,1);
+        [].splice.apply(arr,tmp);
+        
+        // if the array we just added in was empty, we don't want to skip forward the `i` index
+        if (tmp.length == 2) i--;
+      }
+    }
+    
+    return arr;
+	}
 	
 	function create_sandbox() {
     var instanceAPI;
@@ -18,44 +44,86 @@
       ;
       
       function check_pool() {
+        // make sure the pool isn't completed yet
         if (pool !== true) {
+          // check everyone in the pool
           for (var i=0; i<pool.length; i++) {
-            if (pool[i] !== true) return false;
+            if (pool[i] !== true) return false; // bail, not done yet
           }
-          pool = true;
+          pool = true; // flag it as complete
         }
         return (pool === true);
       }
       
       function do_queue() {
         var fn;
-        while (fn = then_queue.shift()) fn();
-        then_queue = true;
+        
+        // make sure at least one `then` callback is registered
+        if (then_queue !== true && then_queue.length) {
+          while (fn = then_queue.shift()) fn(); // empty the queue
+          then_queue = true; // flag it as complete
+        }
       }
       
       chainAPI = {
         and: function() {
+          // can't call 
+          if (then_queue === true || then_queue.length > 0) {
+            throw new Error("Can't call `and()` anymore.");
+            return chainAPI;
+          }
+        
+          var args = flatten_array([].slice.call(arguments)),
+              i
+          ;
+          
+          for (i=0; i<args.length; i++) {
+            (function(fn,pool_idx){ // capture some loop scope
+              // guard the parameter type... must be a function
+              if (!is_func(fn)) throw new Error("Wrong: non-function parameter passed in.");
+            
+              pool[pool_idx] = false;
+              fn(function(){ // pass the trigger function to the callback
+                pool[pool_idx] = true;
+                if (check_pool()) do_queue();
+              });
+              
+            })(args[i],pool.length);
+          }
+        
           return chainAPI;
         },
         then: function(fn) {
-          if (then_queue !== true) then_queue.push(fn);
+          // guard the parameter type... must be a function
+          if (!is_func(fn)) throw new Error("Wrong: non-function parameter passed in.");
+          
+          // are we still adding to the `then` queue?
+          if (then_queue !== true) {
+            then_queue.push(fn);
+            if (check_pool()) do_queue();
+          }
+          // otherwise, the gate's already open, so fire immediately
           else fn();
           
           return chainAPI;
         }
       };
       
-      chainAPI.and.apply({},arguments); // add in arguments passed as constructor parameters
+      // add in arguments passed as constructor parameters
+      chainAPI.and.apply({},arguments);
       
       return chainAPI;
     };
     
+    // rollback to a previous $AG global ref, if any
+    // and return this copy
     instanceAPI.noConflict = function() {
       var new_$AG = global.$AG;
       global.$AG = old_$AG;
       return new_$AG;
     };
     
+    // create a fresh, clean, sandboxed copy of the current $AG
     instanceAPI.sandbox = function() {
       return create_sandbox();
     };
